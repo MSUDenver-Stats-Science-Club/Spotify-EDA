63d6b6c650ebf530c48ae6669eba9a136a9af8f2
Jason Carey <jasoncareyco95@outlook.com>
2022-03-07 11:42:34 -0700

    Modify search function

--- R/spotify_search.R
+++ R/spotify_search.R
@@ -15,17 +15,36 @@ spotify_search <- function(
   ## Validate provided values
   track_name_c <- convert_spaces(track_name)
   artist_name_c <- convert_spaces(artist_name)
-  album_name_c <- convert_spaces(ablum_name)
+  album_name_c <- convert_spaces(album_name)
+
+  if (!is.null(track_name_c)) {
+    track_param <- glue::glue("track:{track_name_c}")
+  } else {
+    track_param <- NULL
+  }
+
+  if (!is.null(artist_name_c)) {
+    artist_param <- glue::glue("artist:{artist_name_c}")
+  } else {
+    artist_param <- NULL
+  }
+
+  if (!is.null(album_name_c)) {
+    album_param <- glue::glue("album:{album_name_c}")
+  } else {
+    album_param <- NULL
+  }
 
   ## Defining our base url
   api_url <- "https://api.spotify.com/v1/search"
 
   ## Create search query
-  # types <- paste(type, collapse = ",")
-  # convert_name <- stringr::str_replace_all(name, " ", "%20")
-  s_params <- glue::glue("track:{track_name_c}+artist:{artist_name_c}+album:{album_name_c}")
+  s_params <- paste(track_param, artist_param, album_param, sep = "+") %>%
+    stringr::str_remove("\\+$")
 
-  types <-
+  types <- stringr::str_extract_all(s_params, "track|artist|album") %>%
+    unlist() %>%
+    paste(collapse = ",")
 
   search_query <- paste0(
     api_url,
@@ -42,8 +61,7 @@ spotify_search <- function(
   if (request$status_code == 200) {
 
     request_list <- request %>%
-      httr::content() %>%
-      purrr::pluck("tracks", "items")
+      httr::content()
 
     request_list %>%
       jsonlite::write_json("C:/Users/jason/Downloads/Datasets/test.json", pretty = TRUE)
